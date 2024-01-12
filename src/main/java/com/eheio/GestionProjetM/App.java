daebc2d6c6c7ec41b8707d4358265c4a3e42f837
YAJBAR rafik <rafikyajbar75@gmail.com>
2024-01-12 18:42:40 +0100

    initisalise le projet

---
+++ src/main/java/com/eheio/GestionProjetM/App.java
@@ -0,0 +1,13 @@
+package com.eheio.GestionProjetM;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
